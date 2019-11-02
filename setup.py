1a753fb64adf48461161e0a1697fbc95c19be3db
Piotr Czajka <czajka@protonmail.com>
2019-11-02 23:31:08 +0100

    bump version

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ginkooo/yapq',
-    version='0.2.10',
+    version='0.3.0',
     zip_safe=False,
 )
